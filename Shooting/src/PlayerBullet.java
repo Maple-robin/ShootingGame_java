d458fb8105b6c86b1a9aafc3d8e7aaebe2beb130
akimarufanboy <zeal23418140@fsgcl.jp>
2024-06-17 11:39:19 +0900

    練習1

--- Shooting/src/PlayerBullet.java
+++ Shooting/src/PlayerBullet.java
@@ -1,5 +1,9 @@
 
 public class PlayerBullet extends Character{
+	public void draw(MyFrame f) {
+		f.setColor(200, 200, 200);
+		f.fillOval(x+5, y, 20, 20);
+	}
 	public PlayerBullet(double x,double y,double vx,double vy) {
 		super(x,y,vx,vy);
 	}
